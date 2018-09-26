7c9ccf675377d3bb46232a2c99ee9b0c7924fe2d
Nick <33632340+NikoHero@users.noreply.github.com>
2018-09-26 12:15:47 +0200

    Add files via upload
    Name change

---
+++ Task3 17601321/17601321-Gade-A1/Building.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17601321_Gade_A1
+{
+    class Building
+    {
+        protected int XPosition;
+        protected int YPosition;
+        protected int Health;
+        protected string team;
+        protected string Symbol;
+
+        //New additions
+        protected int Corruption;
+        protected int WoodAmount;
+        protected int MetalAmount;
+
+
+        public Building()
+         {
+         }
+
+        public abstract void Death();
+        public abstract override string ToString();
+
+
+
+
+    }
+}
---
+++ Task3 17601321/17601321-Gade-A1/EnemyMUnit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17601321_Gade_A1
+{
+    class EnemyMUnit : Unit
+    {
+        public EnemyMUnit(int XPosition, int YPosition, int Health, int MaxHealth, int Speed,
+           int Attack, int AttackRange, string team, string symbol, int IsAttacking, String Name,int DarknessLvl)
+        {
+            this.XPosition = XPosition;
+            this.YPosition = YPosition;
+            this.Health = Health;
+            this.MaxHealth = MaxHealth;
+            this.Speed = Speed;
+            this.Attack = Attack;
+            this.AttackRange = AttackRange;
+            this.team = team;
+            this.Symbol = symbol;
+            this.IsAttacking = IsAttacking;
+            this.Name = Name;
+
+            //Unique to Enemy
+            this.DarknessLvl = DarknessLvl;
+
+        }
+        }
+}
